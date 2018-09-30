225f82949befbc0d02cb9114294d8244ec727d30
James Stuckey <jstuckey@rutgers.edu>
2018-09-30 16:31:07 -0400

    made title of list unique

--- models/list.js
+++ models/list.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 const listSchema = new mongoose.Schema({
-    title: { type: String, required: true },
+    title: { type: String, required: true, unique: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie', required: true }]
 });
